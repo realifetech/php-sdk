0d7441acf64f512f6060dbdb0e74b0c0f72c6a2f
yaroslav <yaroslav.petrov.dev@gmail.com>
2021-06-16 14:04:53 +0300

    BE-1141: added register and authorise to UserClient

--- src/UserManagement/UserClient.php
+++ src/UserManagement/UserClient.php
@@ -3,13 +3,49 @@
 
 namespace LiveStyled\UserManagement;
 
+use GuzzleHttp\Exception\BadResponseException;
 use LiveStyled\Client;
 use LiveStyled\CrudClient;
+use LiveStyled\Exception\EntityCreationException;
 
 class UserClient extends Client implements CrudClient
 {
     protected function getPath()
     {
         return '/v4/user_management/users';
+    }
+
+    public function register($data): array
+    {
+        try {
+            $response = $this->httpClient->post(
+                $this->getPath().'/register',
+                [
+                    'body' => json_encode($data),
+                    'headers' => $this->getHeaders(),
+                ]
+            );
+        } catch (BadResponseException $e) {
+            throw new EntityCreationException($e->getMessage(), $e->getCode(), $e);
+        }
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    public function authorise(int $id, $data): array
+    {
+        try {
+            $response = $this->httpClient->post(
+                $this->getPathWithId($id).'/authorise',
+                [
+                    'body' => json_encode($data),
+                    'headers' => $this->getHeaders(),
+                ]
+            );
+        } catch (BadResponseException $e) {
+            throw new EntityCreationException($e->getMessage(), $e->getCode(), $e);
+        }
+
+        return json_decode($response->getBody()->getContents(), true);
     }
 }
